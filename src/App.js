2e7432ed33051d8cab55c360c7baf8545622c658
saanalapinkangas <118981440+saanalapinkangas@users.noreply.github.com>
2023-11-10 13:34:44 +0200

    Lisätty: ajankohtaista-osio, mediakortit, footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
+import Ajankohtaista from './ajankohtaista';
+import Footer from './footer';
 import Header from './header';
 
 function App() {
   return (
-    <>
+    <div className='start'>
     <Header />
-    </>
+    <div className='sisalto'>
+      <h1>Ajankohtaista juuri nyt</h1>
+      <Ajankohtaista />
+    </div>
+    <Footer />
+    </div>
   );
 }
 
